691d161cad0a87edf19ee3a51b5c345b6f97b13e
lebao2612 <btaldforever@gmail.com>
2026-02-23 09:39:44 +0700

    fix: rebuild app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={< Register />} />
 
-        {/* Protected routes */}
+        {/* Protected routes  */}
         <Route
           path="/"
           element={
